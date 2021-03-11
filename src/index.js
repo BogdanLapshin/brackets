dc407c41df741ee04ae852554da1eb61f8f58339
BogdanLapshin <bogdanizizmaila@gmail.com>
2021-03-11 11:00:06 +0200

    feat: edit the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    if (str.length % 2 == 0) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (str.includes(bracketsConfig[i][0] + bracketsConfig[i][1])) {
+                str = str.replace(bracketsConfig[i][0] + bracketsConfig[i][1], "");
+                console.log(str);
+                i = -1;
+            }
+        }
+    }
+    if (str.length != 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
